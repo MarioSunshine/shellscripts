fa244d61f4d64eed2870c4eea9d0e4a420c020ac
MarioSunshine <138918493+MarioSunshine@users.noreply.github.com>
2023-07-29 18:49:00 -0500

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-x=10 #NOT x = 10 no spaces
-X=20 #variables are case sensitive
-y= #NULL variable
-echo "x = $x"
-echo "X = $X"
-echo "y = $y"
+x=10 #This line assigns the value 10 to x
+X=20 #This line assigns the value 20 to X
+y= #This line assigns an empty/NULL value to y
+echo "x = $x" #This line prints the assigned value of x
+echo "X = $X" #This line prints the assigned value of X
+echo "y = $y" #This line prints the assigned value of y

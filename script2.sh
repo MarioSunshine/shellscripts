2413031e6212c5ae4b4a03f9bfdf6caa21b6bf61
MarioSunshine <138918493+MarioSunshine@users.noreply.github.com>
2023-07-29 18:45:43 -0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# example of using arguments to a script
-echo "My first name is $1"
-echo "My surname is $2"
-echo "Total number of arguments is $#"
+echo "My first name is $1" #This line prints the first argument passed to the script after the text "My first name is"
+echo "My surname is $2" #This line prints the second argument passed to the script after the text "My surname is"
+echo "Total number of arguments is $#" #This line prints the total number of arguments passed to the script using the special variable "$#".

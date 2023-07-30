30802046f5b747b35ffc55e994faac26dc0338bb
MarioSunshine <138918493+MarioSunshine@users.noreply.github.com>
2023-07-29 19:05:37 -0500

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-input =“./names.txt”
-while IFS=, read -r first last; # notice the comma after the equal sign
-do
-name="$first $last"
-username="${first:0:1}$last”
-n=`egrep –c $username /etc/passwd`
-n=$((n+1))
-username=$username$n
-useradd -m $username -s /bin/bash
+input="./names.txt" #Removed space after input,
+while IFS=, read -r first last; do
+    name="$first $last"
+    username="${first:0:1}$last"
+    n=$(egrep -c "$username" /etc/passwd) #Used $() for command substitution instead of backticks in the n=$(egrep -c "$username" /etc/passwd) line.
+    n=$((n+1))
+    username="$username$n" #Added quotes to be read as variable.
+    useradd -m "$username" -s /bin/bash #added quotes to recogine variable.
 done < "$input"
 tail /etc/passwd
